93d63b56c4b57afd5f0ad64de66484b4f1e37aeb
Oscar Love <olabradalove@gmail.com>
2025-04-22 11:22:25 -0700

    Add logging interface

--- internal/common/logging/logger.go
+++ internal/common/logging/logger.go
@@ -1,1 +1,46 @@
 package logger
+
+import (
+	"fmt"
+        "log/slog"
+	"os"
+)
+
+var logger *slog.Logger
+
+func SetupLogger() {
+        // TODO: Load logging info form config file and cli options
+        logger = slog.New(slog.NewTextHandler(os.Stdout, nil))
+}
+
+func Debug(msg string, args ...any) {
+	logger.Debug(msg, args...)
+}
+
+func Debugf(format string, args ...any) {
+	logger.Debug(fmt.Sprintf(format, args...))
+}
+
+func Info(msg string, args ...any) {
+	logger.Info(msg, args...)
+}
+
+func Infof(format string, args ...any) {
+	logger.Info(fmt.Sprintf(format, args...))
+}
+
+func Error(msg string, args ...any) {
+	logger.Error(msg, args)
+}
+
+func Errorf(format string, args ...any) {
+	logger.Error(fmt.Sprintf(format, args...))
+}
+
+func Warn(msg string, args ...any) {
+        logger.Warn(msg, args...)
+}
+
+func Warnf(format string, args ...any) {
+        logger.Warn(fmt.Sprintf(format, args...))
+}
